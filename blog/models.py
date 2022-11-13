dafc1b46679c3ac0da6b889e247dff8155fda933
SergeyTatarenkov <tatarenkovsa@gmail.com>
2022-11-13 12:30:22 +1200

    Personal_portfolio_add_BlogApp

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField("Заголовок: ", max_length=200)
+    text = models.TextField("Текст поста: ", max_length=1000)
+    date = models.DateField("Дата: ")
